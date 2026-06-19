01040e76cc899ff49a1295d65c75315c2022386c
Ronaldo S. <ronaldosm276@gmail.com>
2026-06-19 18:45:17 -0300

    feito js inicial e link

--- js/script_pessoa.js
+++ js/script_pessoa.js
@@ -7,3 +7,19 @@ const idade = $('#div-lista-pessoas')
 // DECLARAR UM ARRAY DE OBJETOS PESSOAS, PARA SALVAR INFORMAÇÕES com colchetes [0,1,2,3...]
 
 const pessoas = []
+
+//capturar evento do submit do formulário
+formPessoa.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const dadosForm = new FormData(formPessoa)
+
+    const pessoa = {
+        nome: dadosForm.get('nome'),
+        idade: dadosForm.get('idade'),
+        renda: dadosForm.get('renda')
+
+        
+    }
+
+})
